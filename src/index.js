6780796180f0121c8368c8e4d4b825357dda40e4
josRosario <jrosario@intellisysdcorp.com>
2024-08-23 17:49:19 -0400

    product

---
+++ src/index.js
@@ -0,0 +1,35 @@
+import express from "express";
+import dotenv from "dotenv";
+//import db from "./config/connection.js";
+//import router from "./route/route.js";
+import bodyParser from "body-parser";
+import kafkaConfig from "./config/kafkaConfig.js"; 
+dotenv.config(); 
+
+const app = express();
+app.use(express.json());
+app.use(bodyParser.urlencoded({extended:true}));
+//app.use(router)
+
+
+const port = process.env.PORT || 4001
+
+const ka = new kafkaConfig();
+ka.consume('my-topic', (value) => {
+    console.log('this is the value, from the receiver', value);
+})
+
+
+/*try {
+    await db.authenticate();
+    await db.sync({alter:true});
+    console.log("The connection to potgres has been stablished...")
+} catch (error) {
+    console.log("It has been an error to stablish the connection...", error)
+    
+}
+*/
+
+app.listen(port, ( ) => {
+    console.log('app running on port: ', port);
+})
